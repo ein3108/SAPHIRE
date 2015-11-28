673b02708eff71e10f4090f1e81ed51125318a52
ein3108 <ein3108@gmail.com>
2015-11-28 07:12:39 +0000

    Conflict with the running Apache2 server and Amazon ESB's start sequence resolved

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var ip = require('ip').address();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'SAPHIRE', version: '1.0' });
+  res.json(ip);
 });
 
 module.exports = router;
--- routes/simulators.js
+++ routes/simulators.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+//var app = require('express')();
+//var app = express();
+//var app = express().createServer();
+//var http = require('http').Server(app);
+//var io = require('socket.io')(http);
+//var ip = require('ip').address();
 
 
 /* GET users listing. */
@@ -35,13 +38,14 @@ router.delete('/deleteSimulator', function(req, res) {
   });
 });
 
+/*
 io.on('connection', function(socket) {
   console.log('a client connected');
   socket.on('simulate', function(data) {
     io.emit('broad', data);
   });
 });
-
-http.listen(4200);
+*/
+//http.listen(4200);
 
 module.exports = router;
