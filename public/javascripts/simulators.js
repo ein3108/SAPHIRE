79e3ceeaa47e0f6a377eef31f24601eaed2bb3f0
ein3108 <ein3108@gmail.com>
2015-12-07 23:08:26 -0800

    iRobot command incorporated & several notification strings are generated upon task updates

--- public/javascripts/simulators.js
+++ public/javascripts/simulators.js
@@ -1,8 +1,10 @@
-//var socket = io.connect('http://127.0.0.1:4200');
-var socket = io.connect('http://ec2-52-25-254-206.us-west-2.compute.amazonaws.com:4200');
+var socket = io.connect('http://127.0.0.1:4200');
+//var socket = io.connect('http://ec2-52-25-254-206.us-west-2.compute.amazonaws.com:4200');
 
 // The timeout value has to be pulled from the database
 var cookTime = 3000;
+var HOST = 'ws://192.168.0.23:';
+var PORT = 2222;
 
 socket.on('connect', function(data) {
   socket.emit('join', 'simulators');
@@ -13,6 +15,8 @@ socket.on('broad', function(data) {
     $('#microwaveImg').prop('src', '/images/Microwave.jpg');
     $('#simBtn1').prop('value', 'Start the microwave');
     $('#microwaveHiddenState').prop('value', 'microwave:start');
+
+    socket.emit('finish', 'Microwave cooking completed');
   } else if (data === 'microwave:start') {
     $('#microwaveImg').prop('src', '/images/microwaveOn.jpg');
     $('#simBtn1').prop('value', 'Stop the microwave');
@@ -21,30 +25,37 @@ socket.on('broad', function(data) {
     setTimeout(simulateMicrowave, cookTime);
   } else if (data === 'fridge:egg') {
     // search the database and send the response to GlassHouse
+    socket.emit('finish', 'There are 4 eggs in the refrigerator');
   } else if (data === 'fridge:bacon') { 
     // search the database and send the response to GlassHouse
+    socket.emit('finish', 'There is roughly 6 slices of bacon left in the refrigerator');
   } else if (data === 'light:stop') { 
     $('#lightImg').prop('src', '/images/TableLamp.jpg');
     $('#simBtn4').prop('value', 'Turn on the light');
     $('#lightHiddenState').prop('value', 'light:start');
+    socket.emit('finish', 'Light\' off');
   } else if (data === 'light:start') { 
     $('#lightImg').prop('src', '/images/TableLampOn.jpg');
     $('#simBtn4').prop('value', 'Turn off the light');
     $('#lightHiddenState').prop('value', 'light:stop');
+    socket.emit('finish', 'Light\' on');
   } else if (data === 'roomba:stop') { 
     $('#irobotImg').prop('src', '/images/Roomba.jpg');
     $('#simBtn5').prop('value', 'Start the roomba');
     $('#irobotHiddenState').prop('value', 'roomba:start');
-  } else if (data === 'roomba:start') { 
+    socket.emit('finish', 'Roomba finished cleaning');
+  } else if (data === '\"roomba:start\"') { 
     $('#irobotImg').prop('src', '/images/irobot.gif');
     $('#simBtn5').prop('value', 'Stop the roomba');
     $('#irobotHiddenState').prop('value', 'roomba:stop');
     // For how long does iRobot run?
+    startIRobot();
     setTimeout(simulateIRobot, 3000); 
   } else if (data === 'washer:stop') { 
     $('#washerImg').prop('src', '/images/Washer.jpg');
     $('#simBtn3').prop('value', 'Start the washer');
     $('#washerHiddenState').prop('value', 'washer:start');
+    socket.emit('finish', 'Laundry done');
   } else if (data === 'washer:start') { 
     $('#washerImg').prop('src', '/images/washer.gif');
     $('#simBtn3').prop('value', 'Start the washer');
@@ -55,16 +66,7 @@ socket.on('broad', function(data) {
   } else {
   }
 
-  socket.emit('finish', 'Simulator state changed');
 });
-
-/*
-$('#simBtn1').on('click', function(e) {
-  alert('in');
-  var state = $('#microwaveHiddenState').val();
-  socket.emit('simulate', state);
-});
-*/
 
 var simulateMicrowave = function() {
   var state = $('#microwaveHiddenState').val();
@@ -89,4 +91,15 @@ var simulateRefrigerator = function() {
 var simulateIRobot = function() {
   var state = $('#irobotHiddenState').val();
   socket.emit('simulate', state);
+};
+
+function startIRobot() {
+  var socket = new WebSocket(HOST + PORT);
+  socket.onopen = function() {
+    socket.send('irobot:start');
+  };
+
+  socket.onclose = function() {
+
+  };
 };
